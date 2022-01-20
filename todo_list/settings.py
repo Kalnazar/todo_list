f6fa41b224649ddfbccf90eeced55f7c6484c813
Kalnazar <74485557+Kalnazar@users.noreply.github.com>
2022-01-20 23:50:31 +0600

    Update settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -76,14 +76,12 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'ToDoList',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'Sayat528152',
-        # 'HOST': 'localhost',
-        # 'PORT': '5432'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ToDoList',
+        'USER': 'postgres',
+        'PASSWORD': 'Sayat528152',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
